d4dc7302efc93ab63bd28c6286cd262f6e0522df
Александр <westerovs@mail.ru>
2021-07-23 22:07:03 +0300

    create template

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,137 @@
-const render = (container, template, place = 'beforeend') => {
-    if (container instanceof Element) {
-        container.insertAdjacentHTML(place, template)
-    }
-}
-console.log('test template')
+import Select  from './select.js';
 
+const select = new Select({
+    selector: '#select',
+    label: 'Выберите технологию',
+    url: 'https://private-c6a57b-selects1.apiary-mock.com/work-hours',
+    onSelect(selectedItem) {
+        console.log(selectedItem);
+    }
+});
+
+select.init()
+
+const actions = document.querySelector('.actions');
+actions.addEventListener('click', e => {
+    switch (e.target.dataset.type) {
+        case 'open':
+            // select.open();
+            break;
+        case 'close':
+            // select.close();
+            break;
+        case 'destroy':
+            // select.destroy();
+            break;
+        case 'get':
+            // if (select.selectedItem) {
+            //     printDataToLog('#log', select.selectedItem.label);
+            // }
+            break;
+        case 'set':
+            // select.selectByIndex(5);
+            break;
+
+        case 'clear':
+            // select.clear();
+            break;
+
+        default:
+            break;
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
+//
+// const select = document.querySelector('.select')
+// const selectHeader = select.querySelector('.select__header')
+//
+//
+// const openSelect = () => {
+//     select.classList.toggle('hide')
+// }
+//
+// const closeSelect = () => {
+//     select.classList.toggle('hide')
+// }
+//
+//
+//
+//
+//
+// const init = () => {
+//     selectHeader.addEventListener('click', openSelect)
+// }
+//
+// init()
+//
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const render = (container, template, place = 'beforeend') => {
+//     if (container instanceof Element) {
+//         container.insertAdjacentHTML(place, template)
+//     }
+// }
