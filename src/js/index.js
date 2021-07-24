45cae4d8d605c8fd72ecfec1e1eebb92f7d6307b
Александр <westerovs@mail.ru>
2021-07-24 12:24:21 +0300

    added checkout items

--- src/js/index.js
+++ src/js/index.js
@@ -1,33 +1,36 @@
 const actions = document.querySelector('.actions')
 
-actions.addEventListener('click', e => {
-    switch (e.target.dataset.type) {
-        case 'open':
-            // select.open()
-            break
-        case 'close':
-            // select.close()
-            break
-        case 'destroy':
-            // select.destroy()
-            break
-        case 'get':
-            // if (select.selectedItem) {
-            //     printDataToLog('#log', select.selectedItem.label)
-            // }
-            break
-        case 'set':
-            // select.selectByIndex(5)
-            break
-
-        case 'clear':
-            // select.clear()
-            break
-
-        default:
-            break
-    }
-})
+// actions.addEventListener('click', e => {
+//     switch (e.target.dataset.type) {
+//         case 'create':
+//             createSelect()
+//             break
+//         case 'open':
+//             // openSelect()
+//             break
+//         case 'close':
+//             // closeSelect()
+//             break
+//
+//         case 'get':
+//             // if (select.selectedItem) {
+//             //     printDataToLog('#log', select.selectedItem.label)
+//             // }
+//             break
+//         case 'set':
+//             // select.selectByIndex(5)
+//             break
+//
+//         case 'clear':
+//             // select.clear()
+//             break
+//         case 'destroy':
+//             destroySelect()
+//             break
+//         default:
+//             break
+//     }
+// })
 
 
 
