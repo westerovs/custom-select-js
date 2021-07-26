49d69f48955e1119b080524a78282a645e326893
Александр <westerovs@mail.ru>
2021-07-26 09:38:55 +0300

    added second zero to end time

--- 1_func-style/src/js/view/tempOptionTime.js
+++ 1_func-style/src/js/view/tempOptionTime.js
@@ -1,8 +1,11 @@
 const createTempOptionTime = (time) => {
+    const h = time.split(':')[0]
+    const m = time.split(':')[1]
+    
     return `<li tabindex="0" role="option"
                     class="select__option"
                     aria-label="выбрано время: ${ time }"
-                    data-value="${ time }">${ time }</li>`
+                    data-value="${ time }">${ h } : ${ m === ' 0' ? '00' : m}</li>`
 }
 
 export {
